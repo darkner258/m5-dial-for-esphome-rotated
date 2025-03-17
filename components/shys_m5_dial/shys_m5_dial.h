dc7e6015b5c69a74296af8a601759340b83deead
darkner258 <106254929+darkner258@users.noreply.github.com>
2025-03-17 22:34:21 +0100

    Update shys_m5_dial.h

--- components/shys_m5_dial/shys_m5_dial.h
+++ components/shys_m5_dial/shys_m5_dial.h
@@ -334,7 +334,7 @@ namespace esphome
       */
       void turnRotaryLeft(){
         m5DialDisplay->resetLastEventTimer();
-        M5Dial.Speaker.tone(5000, 0);
+        M5Dial.Speaker.tone(5000, 1);
 
         if(m5DialDisplay->isDisplayOn()){
           devices[currentDevice]->onRotary(*m5DialDisplay, ROTARY_LEFT);
@@ -348,7 +348,7 @@ namespace esphome
       */
       void turnRotaryRight(){
         m5DialDisplay->resetLastEventTimer();
-        M5Dial.Speaker.tone(5000, 0);
+        M5Dial.Speaker.tone(5000, 1);
 
         if(m5DialDisplay->isDisplayOn()){
           devices[currentDevice]->onRotary(*m5DialDisplay, ROTARY_RIGHT);
